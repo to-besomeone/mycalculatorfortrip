50c82d790914b4de249b7de7bec4430dcab76d1b
to-besomeone <evillion99@gmail.com>
2023-06-19 00:45:59 +0900

    encrypt password

---
+++ src/main/java/com/example/demo/app/core/config/JasyptConfig.java
@@ -0,0 +1,33 @@
+package com.example.demo.app.core.config;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.springframework.beans.factory.annotation.Value;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEncryptableProperties
+public class JasyptConfig {
+
+//    @Value("${jasypt.encryptor.password}")
+//    public String ENV_JASYPT_KEY;
+    public static final String ENV_JASYPT_KEY = System.getenv("enckey");
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+
+        config.setPassword(ENV_JASYPT_KEY);
+        config.setAlgorithm("PBEWithMD5AndTripleDES");
+        config.setKeyObtentionIterations(1000);
+        config.setPoolSize(1);
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+}
---
+++ src/test/java/com/example/demo/app/core/config/JasyptConfigTest.java
@@ -0,0 +1,50 @@
+package com.example.demo.app.core.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@TestConfiguration()
+class JasyptConfigTest {
+
+    private StringEncryptor encryptor;
+
+    @BeforeEach
+    void setUp() {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(JasyptConfig.class);
+        encryptor = context.getBean(StringEncryptor.class);
+    }
+
+    @Test
+    void check_env() {
+        // when
+        String encryptionKey = System.getenv("");
+
+        // then
+        assertThat(encryptionKey).isNotNull();
+        assertThat(encryptionKey).isNotEmpty();
+    }
+
+    @Test
+    void test_encrypt() {
+        // given
+        String source = "calculatoradmin";
+
+        // when
+        String encrypted = encryptor.encrypt(source);
+        String decrypt = encryptor.decrypt(encrypted);
+
+        System.out.println(encrypted);
+        System.out.println(decrypt);
+
+        // then
+        assertThat(encrypted).isNotNull();
+        assertThat(encrypted).isNotEmpty();
+        assertThat(decrypt).isEqualTo(source);
+    }
+
+}
